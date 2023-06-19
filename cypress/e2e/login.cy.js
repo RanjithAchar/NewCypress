03f9c9599f759ec89e156084a100dd597fb3786b
ranjithAchar <ranjithachar42@gmail.com>
2023-06-19 11:12:31 +0530

    login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,12 +2,16 @@
 import login from "../support/pageObject/login"
 // import "cypress/support/commands.js"
 
-const loginMerchant =new login()
+const loginMerchant = new login()
 
-describe('Automating the Login process', ()=>{
-    beforeEach('API',()=>{
-        cy.intercept('POST', 'https://members-qa.coyni.com/api/v2/user/signin').as('signinData'); 
+describe('Automating the Login process', () => {
+    beforeEach('API', () => {
+        cy.intercept('POST', 'https://members-qa.coyni.com/api/v2/user/signin').as('signinData');
         cy.intercept('POST', 'https://members-qa.coyni.com/api/v2/user/step-up').as('stepup');
+        cy.intercept('POST', 'https://members-qa.coyni.com/api/v2/wallets/wallet').as('merchantWallet');
+       cy.intercept('POST', 'https://members-qa.coyni.com/api/v2/wallets/wallet').as('reserveWallet');
+       
+        
     })
     it('1.User can able to navigate to the merchant login page and verifying the url and also the entire description in screen', () => {
         loginMerchant.loginDescription()
@@ -18,7 +22,7 @@ describe('Automating the Login process', ()=>{
         loginMerchant.login(userData.email, userData.password);
         loginMerchant.elements.invalidEmailerror()
         loginMerchant.elements.eyeIcon()
-        
+
     })
     it("User navigating to the login page and passing blank email and password", () => {
         const userData = require('../fixtures/userdata.json')[1];
@@ -26,7 +30,7 @@ describe('Automating the Login process', ()=>{
         loginMerchant.elements.pleaseEnterEmail()
         loginMerchant.elements.pleaseEnterPassword()
     })
-    
+
     it("User navigating to the login page and passing valid email and invalid password ", () => {
         const userData = require('../fixtures/userdata.json')[2];
         loginMerchant.login(userData.email, userData.password);
@@ -42,7 +46,7 @@ describe('Automating the Login process', ()=>{
         const userData = require('../fixtures/userdata.json')[4];
         loginMerchant.login(userData.email, userData.password);
     })
-    it('Verifying the sign in API ',()=>{
+    it('Verifying the sign in API ', () => {
         cy.wait('@signinData').then((interception) => {
             const requestBody = interception.request.body;
             const responseBody = interception.response.body;
@@ -52,33 +56,82 @@ describe('Automating the Login process', ()=>{
             cy.wrap(responseBody.data.email).should('contain', 'qa105@yopmail.com')
             cy.wrap(responseBody.data.firstName).should('contain', 'Kalyan')
             cy.wrap(responseBody.data.lastName).should('contain', 'Kumar')
-            cy.wrap(responseBody.data.userId).should('eq', 2264);
+            cy.wrap(responseBody.data.userId).should('eq', 2497);
             cy.wrap(interception.response.statusCode).should('eq', 200);
             cy.wrap(responseBody.status).should('eq', 'SUCCESS');
-          });
-        }) 
-    it("User navigated to the 2 step authentication screen and validating the entire description", ()=>{
+        });
+    })
+    it("User navigated to the 2 step authentication screen and validating the entire description", () => {
         loginMerchant.twoStepDescription()
     })
-    it('User should click on get SMS button and navigate to phone verification screen and validating the entire description',()=>{
+    it('User should click on get SMS button and navigate to phone verification screen and validating the entire description', () => {
         loginMerchant.phoneVerification()
     })
-    it('User is validating with invalid and valid OTP',()=>{
+    it('User is validating with invalid and valid OTP', () => {
         const userData = require('../fixtures/userdata.json')[5]
         const userData1 = require('../fixtures/userdata.json')[6]
-        loginMerchant.otp(userData.otp);
-        loginMerchant.elements.verificationFailed()
-        loginMerchant.otp(userData1.otp1)
+        // loginMerchant.otp(userData.otp);
+        // loginMerchant.elements.verificationFailed()
+        loginMerchant.otp(userData1.otp1)  
+        cy.wait(300)
+        cy.wait('@stepup').then((interception) => {
+            const requestBody = interception.request.body;
+            const responseBody = interception.response.body;
+            cy.wrap(requestBody).should('exist');
+            cy.wrap(interception.response.statusCode).should('eq', 200);
+            cy.wrap(responseBody.status).should('eq', 'SUCCESS');
+        })
         
     })
-    it('Verifying the Step Up API',()=>{
-        cy.wait('@stepup').then((interception)=>{
-        const requestBody = interception.request.body;
-        const responseBody = interception.response.body;
-        cy.wrap(requestBody).should('exist');
-        cy.wrap(interception.response.statusCode).should('eq', 200);
-        cy.wrap(responseBody.status).should('eq', 'SUCCESS');
+    // it('Verifying the Step Up API', () => {
+        
+    // })
+    // it('Verifying the merchant wallet API', () => {
+    //     cy.wait('@reserveWallet').then((interception) => {
+    //         const requestBody = interception.request.body
+    //         const responseBody = interception.response.body
+    //         cy.wrap(requestBody).should("exist")
+    //         cy.wrap(interception.response.statusCode).should('eq', 200);
+    //         cy.wrap(responseBody.status).should('eq', 'SUCCESS');
+    //         const walletInfo = responseBody.data.walletInfo;
+    //         expect(walletInfo).to.be.an('array').and.have.lengthOf.at.least(1);
+    //         const firstItem = walletInfo[0];
+    //         expect(firstItem.walletName).to.equal('Reserve');
+    //         let availableBalance;
+    //         let walletID
+    //         cy.wrap(firstItem.availabilityToUse).as('availableBalance')
+    //             .then((amount) => {
+    //                 availableBalance = amount;
+    //                 cy.log('Total Merchant balance availabe to use--', availableBalance);
+    //                 cy.wrap(firstItem.walletId).as('merchantwalletID').then((id) => {
+    //                     walletID = id
+    //                     cy.log('Merchant wallet ID---', walletID)
+    //                 })
+    //             })
+    //     })
+    // })
+    it('Verifying the reserve wallet API', () => {
+        cy.wait('@merchantWallet').then((interception) => {
+            const requestBody = interception.request.body
+            const responseBody = interception.response.body
+            cy.wrap(requestBody).should("exist")
+            cy.wrap(interception.response.statusCode).should('eq', 200);
+            cy.wrap(responseBody.status).should('eq', 'SUCCESS');
+            const walletInfo = responseBody.data.walletInfo;
+            expect(walletInfo).to.be.an('array').and.have.lengthOf.at.least(1);
+            const firstItem = walletInfo[0];
+            // expect(firstItem.walletType).to.equal('MERCHANT');
+            let availableBalance;
+            let walletID
+            cy.wrap(firstItem.availabilityToUse).as('availableBalance')
+                .then((amount) => {
+                    availableBalance = amount;
+                    cy.log('Total Reserve balance availabe to use--', availableBalance);
+                    cy.wrap(firstItem.walletId).as('reservewalletID').then((id) => {
+                        walletID = id
+                        cy.log('Reserve wallet ID---', walletID)
+                    })
+                })
         })
     })
 })
-
